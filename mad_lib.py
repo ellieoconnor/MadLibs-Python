ebaef7aebadc9354ff1e58992baa6897809e94bb
Ellie O'Connor <87724044+ellieoconnor@users.noreply.github.com>
2021-08-18 06:57:29 -0400

    Update mad_lib.py
    Added a print statement at the beginning to welcome the player.
    Then I added a user input for the player's name and welcomed them personally.
    I tried to clean up the output a bit more by separating each line.

--- mad_lib.py
+++ mad_lib.py
@@ -1,26 +1,27 @@
-"""Let's play a game of MadLibs!
-Fill in the needed words and see your crazy story come to life."""
+print("Let's play a game of MadLibs! \nFill in the needed words and see your crazy story come to life.")
+name = input("\nWhat's your name: ")
+print("\nExcellent " + name +"! Now let's play!")
 
-relative1 = input("First, enter a relative: ")
-adj1 = input("Great! Now enter an adjective: ")
-adj2 = input("Enter another adjective: ")
-adj3 = input("Enter an adjective: ")
-person_in_room1 = input("Now, name a person in the room: ")
-adj4 = input("Enter an adjective: ")
-adj5 = input("Great! Enter one more adjective: ")
-verb_ed1 = input("Enter a verb that ends in -ed: ")
-body_part = input("Now, Enter a body part: ")
-verb_ing1 = input("Enter a verb that ends in -ing: ")
-n_plural1 = input("Enter a plural noun: ")
-n1 = input("Enter a noun: ")
-adverb1 = input("Next, you need to enter an adverb: ")
-verb1 = input("Enter a verb: ")
-verb2 = input("Enter a second verb: ")
-relative2 = input("Enter one more relative: ")
-person_in_room2 = input("Lastly, enter a person in the room: ")
+relative1 = input("\nFirst, enter a relative: ")
+adj1 = input("\nGreat! Now enter an adjective: ")
+adj2 = input("\nEnter another adjective: ")
+adj3 = input("\nEnter an adjective: ")
+person_in_room1 = input("\nNow, name a person in the room: ")
+adj4 = input("\nEnter an adjective: ")
+adj5 = input("\nGreat! Enter one more adjective: ")
+verb_ed1 = input("\nEnter a verb that ends in -ed: ")
+body_part = input("\nNow, Enter a body part: ")
+verb_ing1 = input("\nEnter a verb that ends in -ing: ")
+n_plural1 = input("\nEnter a plural noun: ")
+n1 = input("\nEnter a noun: ")
+adverb1 = input("\nNext, you need to enter an adverb: ")
+verb1 = input("\nEnter a verb: ")
+verb2 = input("\nEnter a second verb: ")
+relative2 = input("\nEnter one more relative: ")
+person_in_room2 = input("\nLastly, enter a person in the room: ")
 
-lib = """Dear {},\n
-I am having a(n) {} time at camp. The counselour is {} and the food is {}. I met {} and we became {} friends. Unfortunately,
+lib = """\nDear {},
+\nI am having a(n) {} time at camp. The counselour is {} and the food is {}. I met {} and we became {} friends. Unfortunately,
 {} is {} and I {} my {} so we couldn't go {} like everybody else. I need more {} and a {} sharpener, so please {} {} more when you
 {} back.\n
 Your {},\n
